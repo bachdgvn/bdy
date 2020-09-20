8c357fb6b9d4b22461c3a58b2e488c1678db9667
Bách Dương <30729887+bachdgvn@users.noreply.github.com>
2020-09-20 09:56:17 +0700

    Update Layout.js

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -32,8 +32,7 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                 <body className={bodyClass} />
             </Helmet>
 
-            <div className="code-injection-head">
-                `${site.codeinjection_head}`
+            <div className="code-injection-head" dangerouslySetInnerHTML={site.codeinjection_head}>
             </div>
 
             <div className="viewport">

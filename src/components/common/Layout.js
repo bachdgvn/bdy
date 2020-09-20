643824b52f9b231010d846ab88bf309f52101d04
Bách Dương <30729887+bachdgvn@users.noreply.github.com>
2020-09-20 09:32:08 +0700

    Update Layout.js

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -20,6 +20,7 @@ import '../../styles/app.css'
 */
 const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
     const site = data.allGhostSettings.edges[0].node
+    console.log('site', site)
     const twitterUrl = site.twitter ? `https://twitter.com/${site.twitter.replace(/^@/, ``)}` : null
     const facebookUrl = site.facebook ? `https://www.facebook.com/${site.facebook.replace(/^\//, ``)}` : null
 
